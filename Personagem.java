f239d7c8194d1c45e45d5a754e2632ab1acd2e59
Gabriel-Andradeprog30 <157922939+Gabriel-Andradeprog30@users.noreply.github.com>
2026-05-04 10:19:49 -0300

    Update Personagem.java
    atualizado

--- Personagem.java
+++ Personagem.java
@@ -1,4 +1,5 @@
 public class Personagem {
+    // Atributos básicos do personagem
     String nome;
     String classe;
     int nivel;
@@ -7,18 +8,21 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this. poderBase = poderBase;
     }
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
 
+    void exibirStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
+        System.out.println(); // linha em branco para separar personagens
     }
+
+   
 }
-
