00990732bee624a71da2e3d13e2ed9a7292e8510
Gabriel-Andradeprog30 <157922939+Gabriel-Andradeprog30@users.noreply.github.com>
2026-05-04 10:22:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
-
-    }
+  // Método principal para testar a classe
+  public class Main {    
+       public static void main(String[] args) {
+        // Instanciando o primeiro personagem
+        Personagem arqueiro = new Personagem("Legolas", "Arqueiro", 5, 100, 12.5);
+      
+        // Instanciando o segundo personagem
+        Personagem guerreiro = new Personagem("Thorus", "Guerreiro", 7, 120, 15.0);
+      
+        // Exibindo status dos personagens
+        arqueiro.exibirStatus();
+        guerreiro.exibirStatus();
+    }   
 }
